05c156ac144d18e911806963a516b4ea27699d62
Francisco Emanoel <francisco170142329@gmail.com>
2021-05-18 14:52:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 server = express()
 
 server.get('/',(req, res) => {
-    res.send("hello 55")
+    res.send("Hello World")
 })
 
 var PORT = process.env.PORT || 3333
